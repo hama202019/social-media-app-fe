af1dd4966a9617b53b96601be3e1d897ae7c8527
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-03-26 09:43:36 +0300

    Logo and SearchBar Components are created

--- src/components/ProfileSide.jsx
+++ src/components/ProfileSide.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import LogoSearch from './SearchLogo/LogoSearch.jsx'
+import './ProfileSide.css'
 
 const ProfileSide = () => {
   return (
     <div className='ProfileSide'>
-        ProfileSide
+        <LogoSearch />
     </div>
   )
 }

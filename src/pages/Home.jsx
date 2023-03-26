bb9cb9474bf0f9d60b8acb1c5ddf5663f35063fe
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-03-26 07:42:57 +0300

    the home page sections are added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProfileSide from '../components/ProfileSide'
+import './Home.css'
+const Home = () => {
+  return (
+    <div className='Home'>
+        <ProfileSide />
+        <div className="postSide">Post</div>
+        <div className="rightSide">Right</div>
+    </div>
+  )
+}
+
+export default Home

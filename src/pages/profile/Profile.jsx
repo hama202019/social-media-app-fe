f013a5bb9c995cd56de438869edb16e5f5b99a15
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-03-27 10:53:00 +0300

    the profile left side is done

---
+++ src/pages/profile/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProfileLeft from '../../components/profileLeft/ProfileLeft'
+import './Profile.css'
+const Profile = () => {
+  return (
+    <div className='Profile'>
+        <ProfileLeft />
+    </div>
+  )
+}
+
+export default Profile

81db7d52f4d73661bf0ed8bc1ab0f733bad8b44d
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-03-28 07:06:28 +0300

    the signup form is created

---
+++ src/components/signupForm/SignupForm.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './SignupForm.css'
+const SignupForm = () => {
+  return (
+    <div className="SignupForm">
+        <h2>Signup</h2>
+        <div className="fullName">
+            <input type='text' placeholder='firstName' />
+            <input type='text' placeholder='lastName' />
+        </div>
+        <div>
+            <input placeholder='email' type='email' />
+        </div>
+        <div className="password">
+            <input type="password" placeholder='password' />
+            <input type="password" placeholder='confirm password' />
+        </div>
+        <button className='button signupButton'>Signup</button>
+    </div>
+  )
+}
+
+export default SignupForm

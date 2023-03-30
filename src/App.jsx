010f916b6532914888d34b69f7a424a3d24fe370
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-03-30 16:04:50 +0300

    home and auth routes are created, using react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import { useSelector } from "react-redux";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css"
 import Auth from "./pages/auth/Auth";
 import Home from "./pages/Home";
 import Profile from "./pages/profile/Profile";
 function App() {
+  const user = useSelector(state => state.authReducer.authData)
+
   return (
     <div className="App">
         <div className="blur" style={{top: '-18%', right: '0'}}></div>
         <div className="blur" style={{top: '36%', left: '-8rem'}}></div>
-        {/* <Home /> */}
-        {/* <Profile /> */}
-        <Auth />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={user ? <Navigate to={'/home'}/> : <Navigate to="/auth" /> }/>
+            <Route path="/auth" element={user ? <Navigate to="home" /> : <Auth />} />
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }

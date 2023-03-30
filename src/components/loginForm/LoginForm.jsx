f1751334b7a075b4ebdd06cfe82ef2c1d8f12ce1
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-03-30 10:18:03 +0300

    getting data from login fields is done

--- src/components/loginForm/LoginForm.jsx
+++ src/components/loginForm/LoginForm.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './LoginForm.css'
 
 const LoginForm = ({setIsSignUp}) => {
+  const [data, setData] = useState({email: '', password: ''})
+
+  const changeHandler = e => {
+    setData({...data, [e.target.name]: e.target.value})
+  }
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log(data)
+  }
+
   return (
-    <div className="LoginForm">
+    <form className="LoginForm" onSubmit={submitHandler}>
         <h2>Login</h2>
         <div>
-            <input placeholder='email' type='email' />
+            <input placeholder='email' type='email' name='email' onChange={changeHandler}/>
         </div>
         <div className="password">
-            <input type="password" placeholder='password' />
+            <input type="password" placeholder='password' name='password' onChange={changeHandler}/>
         </div>
         <h6 onClick={setIsSignUp}>Don't have an account? <span>Signup</span> </h6>
         <button className='button loginButton'>Login</button>
-    </div>
+    </form>
   )
 }
 

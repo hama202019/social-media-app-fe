c024e9f542830618a43bc8ed798b729abb5c9e16
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-02 10:10:41 +0300

    placeholder pictures are added if the user didn't upload his photos

--- src/components/profileCard/ProfileCard.jsx
+++ src/components/profileCard/ProfileCard.jsx
@@ -1,37 +1,40 @@
 import React from 'react'
-import Cover from '../../assets/img/cover.jpg'
-import Profile from '../../assets/img/profileImg.jpg'
+import Cover from '../../assets/img/coverPlaceHolder.png'
+import Profile from '../../assets/img/profileImgPlaceHolder.png'
 import './ProfileCard.css'
 import { Link, Navigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const ProfileCard = ({profilePage}) => {
-
+    const {postsData} = useSelector(state => state.postsReducer)
+    const user = useSelector(state => state.authReducer.authData)
+    const myOwnPosts = postsData.filter(post => post.userId === user._id)
     return (
     <div className='ProfileCard'>
         <div className='profileImages'>
-            <img src={Cover} />
-            <img src={Profile} />
+            <img src={user.coverPicture || Cover} />
+            <img src={user.profilePicture || Profile} />
         </div>
         <div className="profileName">
-            <span>Jane Eyre</span>
-            <span>Senior Backend Developer</span>
+            <span>{user.firstName + ' ' + user.lastName}</span>
+            <span>{user.about || ""}</span>
         </div>
         <div className="followStatus">
             <hr />
             <div>
                 <div className="follow">
-                    <span>6,890</span>
+                    <span>{user.followers.length}</span>
                     <span>Followers</span>
                 </div>
                 <div className="vl"></div>
                 <div className="follow">
-                    <span>60</span>
+                    <span>{user.following.length}</span>
                     <span>Followings</span>
                 </div>
                 {profilePage && (<>
                     <div className="vl"></div>
                     <div className="follow">
-                        <span>3</span>
+                        <span>{myOwnPosts.length}</span>
                         <span>Posts</span>
                     </div>
                 </>)} 

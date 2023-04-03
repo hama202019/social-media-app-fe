e973d460157dad8d4130c12c539ab4837aa2de4d
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-03 11:22:57 +0300

    follower card is created

--- src/components/followersCard/FollowersCard.jsx
+++ src/components/followersCard/FollowersCard.jsx
@@ -1,23 +1,17 @@
 import React from 'react'
 import classes from './FollowersCard.module.css'
 import { followers } from '../../data/followersData'
+import { useSelector } from 'react-redux'
+import FollowerCard from '../followerCard/FollowerCard'
 
 const FollowersCard = () => {
+    // const {followers} = useSelector(state => state.authReducer.authData)
   return (
     <div className={classes.FollowersCard}>
         <h3> Who is following you </h3>
-        {followers.map((follower, index) => {
+        {followers.map( (follower, idx) => {
             return (
-                <div className={classes.follower} key={index}>
-                    <div>
-                        <img src={follower.img} className={classes.followerImg} />
-                        <div className={classes.name} >
-                            <span>{follower.name}</span>
-                            <span>@{follower.username}</span>
-                        </div>
-                    </div>
-                    <button className='button'>follow</button>
-                </div>
+                <FollowerCard key={idx} follower={follower} classes={classes}/>
             )
         })}
     </div>

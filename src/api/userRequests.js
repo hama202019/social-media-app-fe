6ffd46cdd9c7a06519b67a5a97369d618f3f3546
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-03 11:43:35 +0300

    get user and get all users request functions are crated

--- src/api/userRequests.js
+++ src/api/userRequests.js
@@ -4,3 +4,5 @@ const userAPI = axios.create({ baseURL: 'http://localhost:4000/user' });
 
 export const updateUser = (id, data) => userAPI.put(`/${id}`, data)
 export const followUser = (id, data) => userAPI.put(`/${id}`, data)
+export const getUser = id => userAPI.get(`/${id}`)
+export const getAllUsers = () => userAPI.get('/')

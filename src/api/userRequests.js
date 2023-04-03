70067b2ef116acca60f761211b4bee816ff4441e
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-03 10:46:48 +0300

    a request function to follow user endpoint is created

--- src/api/userRequests.js
+++ src/api/userRequests.js
@@ -3,3 +3,4 @@ import axios from 'axios'
 const userAPI = axios.create({ baseURL: 'http://localhost:4000/user' });
 
 export const updateUser = (id, data) => userAPI.put(`/${id}`, data)
+export const followUser = (id, data) => userAPI.put(`/${id}`, data)

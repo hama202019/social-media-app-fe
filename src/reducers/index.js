5c4490e7fbb5a70a37cf47bf67d3d13836cddbb4
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-04-05 14:58:20 +0300

    chats reducer is added to all reducers to be used in redux store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import authReducer from '../reducers/authReducer'
 import {combineReducers} from 'redux'
 import postsReducer from './postsReducer'
+import chatsReducer from './chatsReducer'
 
-const allReducers = combineReducers({authReducer, postsReducer})
+const allReducers = combineReducers({authReducer, postsReducer, chatsReducer})
 
 export default allReducers
